f598bf04115b269318bff20a78f89e3351d7d0f6
Kyle Klaus <kklaus@indemnity83.com>
2024-04-17 22:55:54 -0700

    add boot and status screen customizations

--- Marlin/_Statusscreen.h
+++ Marlin/_Statusscreen.h
@@ -1,59 +1,29 @@
 /**
- * Marlin 3D Printer Firmware
- * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
- *
- * Based on Sprinter and grbl.
- * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
+ * Made with Marlin Bitmap Converter
+ * https://marlinfw.org/tools/u8glib/converter.html
  */
 #pragma once
 
-/**
- * Custom Status Screen bitmap
- *
- * Place this file in the root with your configuration files
- * and enable CUSTOM_STATUS_SCREEN_IMAGE in Configuration.h.
- *
- * Use the Marlin Bitmap Converter to make your own:
- * https://marlinfw.org/tools/u8glib/converter.html
- */
+#define STATUS_SCREEN_X 88
+#define STATUS_LOGO_WIDTH 40
 
-//
-// Status Screen Logo bitmap
-//
-#define STATUS_LOGO_Y            8
-#define STATUS_LOGO_WIDTH       39
-
-const unsigned char status_logo_bmp[] PROGMEM = {
-  B11111000,B00000001,B10000000,B00000000,B00001100,
-  B01001000,B00000000,B10000000,B00000000,B00010010,
-  B01000011,B11000011,B10001100,B11010000,B00000010,
-  B01110001,B00100100,B10010010,B01100111,B11001100,
-  B01000001,B00100100,B10011110,B01000000,B00000010,
-  B01001001,B00100100,B10010000,B01000000,B00010010,
-  B11111011,B10110011,B11001110,B11100000,B00001100,
-  B00000000,B00000000,B00000000,B00000000,B00000000,
-  B00000000,B00000000,B00000000,B00000000,B00000000,
-  B00000000,B00000011,B10000000,B00000000,B00000000,
-  B00000000,B00000010,B01000000,B00000000,B00000000,
-  B00000000,B00000010,B01010110,B01100000,B00000000,
-  B00000000,B00000011,B10011000,B10010000,B00000000,
-  B00000000,B00000010,B00010000,B10010000,B00000000,
-  B00000000,B00000010,B00010000,B10010000,B00000000,
-  B00000000,B00000010,B00010000,B01100000,B00000000
+const unsigned char custom_start_bmp[] PROGMEM = {
+  B11111000,B00000001,B10000000,B00000000,B00001100, // #####..........##...................##..
+  B01001000,B00000000,B10000000,B00000000,B00010010, // .#..#...........#..................#..#.
+  B01000000,B00000000,B10001100,B11010000,B00000010, // .#..............#...##..##.#..........#.
+  B01100011,B11000111,B10010010,B01100111,B11001100, // .##...####...####..#..#..##..#####..##..
+  B01000001,B00100100,B10011110,B01000000,B00000010, // .#.....#..#..#..#..####..#............#.
+  B01001001,B00100100,B10010000,B01000000,B00010010, // .#..#..#..#..#..#..#.....#.........#..#.
+  B11111011,B10110011,B11001110,B11100000,B00001100, // #####.###.##..####..###.###.........##..
+  B00000000,B00000000,B00000000,B00000000,B00000000, // ........................................
+  B00000000,B11100000,B00000000,B00000000,B00000000, // ........###.............................
+  B00000001,B00010000,B00000001,B00100000,B00000000, // .......#...#...........#..#.............
+  B00000001,B00000011,B00110100,B00100001,B10000000, // .......#......##..##.#....#....##.......
+  B00000000,B11100100,B10011001,B01111010,B01000000, // ........###..#..#..##..#.####.#..#......
+  B00000000,B00010100,B10010001,B00100011,B11000000, // ...........#.#..#..#...#..#...####......
+  B00000001,B00010111,B00010001,B00100010,B00000000, // .......#...#.###...#...#..#...#.........
+  B00000000,B11100100,B00111001,B10011001,B11000000, // ........###..#....###..##..##..###......
+  B00000000,B00000100,B00000000,B00000000,B00000000  // .............#..........................
 };
 
 //
